64f578898b5a16172378524f737149e5f8d99f19
Rory Smith <roarsmith3@gmail.com>
2015-02-17 21:34:39 +0000

    connected it all up

--- js/pusher-code.js
+++ js/pusher-code.js
@@ -1,20 +1,30 @@
 
 		// Load in player Images
     var pusher = new Pusher('bf741a8a4c211bec9b6b');
-	var channel = pusher.subscribe('private-game-id');
+	var channel = pusher.subscribe('private-game-id-'+sessionNum);
 
-	var player1 = {'x':50, 'shouldShoot':false, 'bullets': []};
-	var player2 = {'x':50, 'shouldShoot':false, 'bullets': []};
+	var player1 = {'connected':false,'x':50, 'shouldShoot':false, 'bullets': []};
+	var player2 = {'connected':false,'x':50, 'shouldShoot':false, 'bullets': []};
 
     channel.bind('client-coord-update',function(data) {
     //do something soon
 
     	if(data.player == "1"){
+    		player1.connected = true;
     		player1.x = (data.y + 90) /1.8;
     		player1.shouldShoot = data.touched;
     	}else{
+    		player2.connected = true;
     		player2.x = (data.y + 90) /1.8;
     		player2.shouldShoot = data.touched;
+    	}
+
+    	if(player1.connected && player2.connected){
+    		$('.QRcontainer').fadeOut();
+    	}else if(player1.connected){
+    		$('.QRplayer1').fadeOut();
+    	}else if(player2.connected){
+    		$('.QRplayer2').fadeOut();
     	}
 		
   	});

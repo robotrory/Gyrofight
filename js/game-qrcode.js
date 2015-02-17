611cb6ba7c585b8350c0ecffa13e59915e9ee0a8
BenAllenUK <external@benallen.info>
2015-02-17 20:59:31 +0000

    Added working QR codes

--- js/game-qrcode.js
+++ js/game-qrcode.js
@@ -1,18 +1,34 @@
-// QR Code goes here
-		function generateQR(qrDivName,urlToEncode){
-			alert(urlToEncode);
-			var qrcode = new QRCode(document.getElementById("qrcode1"), {
-			width : 100,
-			height : 100
-			});
-			qrcode.makeCode(urlToEncode);
-		
-		}
-		var sessionNum = Math.floor(Math.random() * 100000);
-		//var x1 = generateQR("qrcode1", "https://smithyproductions.ngrok.com/mobile?sessionNumber=" + sessionNum + "&player=1");
-		//var y1 = generateQR("qrcode2", "https://smithyproductions.ngrok.com/mobile?sessionNumber=" + sessionNum + "&player=2");
-  		var qrcode = new QRCode(document.getElementById("qrcode1"), {
-			width : 100,
-			height : 100
-			});
-			qrcode.makeCode("https://smithyproductions.ngrok.com/mobile?sessionNumber=" + sessionNum + "&player=2");
+/*// QR Code goes here
+function generateQR(urlToEncode){
+	alert(urlToEncode);
+	var qrcode = new QRCode(document.getElementById("#QRplayer1_qr"), {
+	width : 100,
+	height : 100
+	});
+	qrcode.makeCode(urlToEncode);
+
+}
+//var sessionNum = Math.floor(Math.random() * 100000);
+generateQR("https://smithyproductions.ngrok.com/mobile");
+//?sessionNumber=" + sessionNum + "&player=1"
+//var y1 = generateQR("qrcode2", "https://smithyproductions.ngrok.com/mobile?sessionNumber=" + sessionNum + "&player=2");
+/*var qrcode = new QRCode(document.getElementById("QRcode1"), {
+	width : 100,
+	height : 100
+	});
+	qrcode.makeCode("https://smithyproductions.ngrok.com/mobile?sessionNumber=" + sessionNum + "&player=2");
+	
+	*/
+	
+	
+	
+/*	
+	function generateQR(idName,urlToEncode){
+                      var qrcode = new QRCode(document.getElementById("qrcode"), {
+                      width : 100,
+                      height : 100
+                      });
+                      qrcode.makeCode(urlToEncode);
+                      
+                  }
+                 */

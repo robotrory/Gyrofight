098e43306d7454023216a92909c9b41c48bd8a9b
Rory Smith <roarsmith3@gmail.com>
2015-02-17 19:22:56 +0000

    Webserver should now be fully functional

--- webserver.js
+++ webserver.js
@@ -1,8 +1,12 @@
 var express = require( 'express' );
 var Pusher = require( 'pusher' );
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
-var app = express( express.logger() );
-app.use( express.bodyParser() );
+var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var pusher = new Pusher( { appId: '107012', key: 'bf741a8a4c211bec9b6b', secret: '4fe4169269b1c8746e84' } );
 
@@ -11,6 +15,11 @@ app.post( '/pusher/auth', function( req, res ) {
   var channel = req.body.channel_name;
   var auth = pusher.authenticate( socketId, channel );
   res.send( auth );
+} );
+
+app.all('*', function( req, res ) {
+  console.log(req.originalUrl);
+  res.sendfile( __dirname + req.originalUrl );
 } );
 
 var port = 8080;

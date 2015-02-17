9ead50890b2de78fbc444d3b0d4c1a1d382f0f08
Rory Smith <roarsmith3@gmail.com>
2015-02-17 20:10:21 +0000

    Moved moar code

--- js/gamecode.js
+++ js/gamecode.js
@@ -1,21 +1,4 @@
-// QR Code goes here
-		function generateQR(qrDivName,urlToEncode){
-			alert(urlToEncode);
-			var qrcode = new QRCode(document.getElementById("qrcode1"), {
-			width : 100,
-			height : 100
-			});
-			qrcode.makeCode(urlToEncode);
-		
-		}
-		var sessionNum = Math.floor(Math.random() * 100000);
-		//var x1 = generateQR("qrcode1", "https://smithyproductions.ngrok.com/mobile?sessionNumber=" + sessionNum + "&player=1");
-		//var y1 = generateQR("qrcode2", "https://smithyproductions.ngrok.com/mobile?sessionNumber=" + sessionNum + "&player=2");
-  		var qrcode = new QRCode(document.getElementById("qrcode1"), {
-			width : 100,
-			height : 100
-			});
-			qrcode.makeCode("https://smithyproductions.ngrok.com/mobile?sessionNumber=" + sessionNum + "&player=2");
+
 		// Load in player Images
     var pusher = new Pusher('bf741a8a4c211bec9b6b');
 	var channel = pusher.subscribe('private-channel');
